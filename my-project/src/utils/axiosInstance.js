75a6c65ade05cab4830c535e2ec3103044c80c25
Nikhil Patil <nikhilpatil5720@gmail.com>
2025-09-03 11:05:00 +0530

    Update axios instance to use VITE_BACKEND_URL for production environment

--- my-project/src/utils/axiosInstance.js
+++ my-project/src/utils/axiosInstance.js
@@ -12,7 +12,7 @@ const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000/api/v1"
-      : "https://backend-project-1-2hld.onrender.com/api/v1",
+      :  import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
 
